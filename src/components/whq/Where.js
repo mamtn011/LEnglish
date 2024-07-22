500d26a12862709516f2e5f4d0625956f3cea91d
Mobin <mamtn011@gmail.com>
2024-07-23 03:06:04 +0600

    completed where

--- src/components/whq/Where.js
+++ src/components/whq/Where.js
@@ -3,17 +3,176 @@ import React from "react";
 const WhoWhom = () => {
   return (
     <div className="w-full">
-      <button className="btn-lesson"></button>
+      <button className="btn-lesson">Where</button>
       <div className={`flex flex-col gap-4 mb-6`}>
         <div>
           <p className="font-bold py-2">Description:</p>
-          <p className="py-2"></p>
-          <p></p>
+          <div className="py-2">
+            <p>Where = কোথায় / কী অবস্থায়</p>
+          </div>
+          <p>Where-family + auxiliary verb + subject + verb + object</p>
         </div>
         <p className="font-bold">Examples:</p>
         <div>
-          <p>?</p>
-          <p>?</p>
+          <p>তারা কোথায় যাচ্ছে?</p>
+          <p>Where are they going?</p>
+        </div>
+        <div>
+          <p>আমার কোথায় ইংরেজি শেখা উচিৎ?</p>
+          <p>Where should I learn English?</p>
+        </div>
+        <div>
+          <p>তোমাদের কলেজ কোথায় অবস্থিত?</p>
+          <p>Where is your college situated?</p>
+        </div>
+        <div>
+          <p>তুমি কোথায় ছিলে?</p>
+          <p>Where were you?</p>
+        </div>
+        <div>
+          <p>তুমি এতদিন কোথায় ছিলে?</p>
+          <p>Where had you been so long?</p>
+        </div>
+        <div>
+          <p>তুমি কোথায় এই হারামিদের সাথে দেখা করেছিলে?</p>
+          <p>Where did you meet with these devils?</p>
+        </div>
+        <div>
+          <p>
+            যদি তোমার দূর্দিনে আমি তোমাকে সাহায্য না করতাম তবে তুমি আজ কী
+            অবস্থায় থাকতে?
+          </p>
+          <p>Where would you be now if I didn't help you in your rainy days?</p>
+        </div>
+        <div>
+          <p>তুমি কোত্থেকে এসেছ?</p>
+          <p>Where are you come from?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">
+            Whereabouts = কোনখানে / কোন জায়গার কাছে / খোঁজখবর
+          </p>
+          <p>কোনখানে তুমি আমার বন্ধুকে খুঁজে পেয়েছ?</p>
+          <p>Whereabouts have you found out my friend?</p>
+        </div>
+        <div>
+          <p>তুমি কি তার কোন খোঁজখবর জান?</p>
+          <p>Do you know her whereabouts?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">Wherein = কিসে / কোন বিষয়ে</p>
+          <p>কিসে অনেক সমস্যা আছে?</p>
+          <p>Wherein are there many problems?</p>
+        </div>
+        <div>
+          <p>আমার ভুল কিসে?</p>
+          <p>Wherein am I wrong?</p>
+        </div>
+        <div>
+          <p>তুমি কিসে সফলতা পেতে চেষ্টা করছ?</p>
+          <p>Wherein are you trying to gain success?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">The whys and wherefores = কারণসমূহ</p>
+          <p>তুমি কি তার ব্যর্থতার কারণসমূহ জান?</p>
+          <p>Do you know the whys and wherefores of his failure?</p>
+        </div>
+        <div>
+          <p>
+            তোমার কি বাংলাদেশের খেলোয়ারদের ব্যর্থতার কারণসমূহ সম্পর্কে কোন ধারনা
+            আছে?
+          </p>
+          <p>
+            Do you have any idea about the whys and wherefores of the failure of
+            the Bangladeshi players?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereafter = যারপর / যে ঘটনার পর</p>
+          <p>
+            তার বাবা একটি দূর্ঘটনায় মারা গিয়েছিল, যারপর সে অজ্ঞান হয়ে গিয়েছিল?
+          </p>
+          <p>His father died by an accident, whereafter he became senseless?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereas = যেখানে / পক্ষান্তরে</p>
+          <p>সে পরীক্ষায় A+ পেয়েছে, যেখানে তার ভাই ফেল করেছে?</p>
+          <p>He has got A+ in the exam, whereas his brother has failed?</p>
+        </div>
+        <div>
+          <p>ইন্ডিয়া দিন দিন উন্নতি করছে, যেখানে বাংলাদেশ পিছিয়ে পরছে?</p>
+          <p>
+            India is developing day by day, whereas Bangladesh is lagging
+            behind?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">
+            Wherefrom/Whence = যেখান থেকে / যে উৎস থেকে
+          </p>
+          <p>আমি ইংরেজি শেখা শুরু করতে চাই যেখান থেকে তুমি শুরু করতে চাও?</p>
+          <p>
+            I want to start learning English wherefrom/whence you want to start?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">Wheresoever/Wherever = যেখানেই</p>
+          <p>তুমি যেখানেই থাক আমি তোমাকে খুঁজে বের করবো?</p>
+          <p>I will find you out wheresoever/wherever you are?</p>
+        </div>
+        <div>
+          <p>তুমি যেখানেই যাবে আমি সেখানেই যাবো?</p>
+          <p>I will go there wheresoever you will go?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereon = যার উপর</p>
+          <p>আমি বিষয়টি বুঝতে চাই, যার উপর তুমি এত মনোযোগ দিয়েছ?</p>
+          <p>
+            I want to understand the matter whereon you have paid much
+            attention?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereupon/Whereat = যার ফলে</p>
+          <p>
+            সে একটি বিতর্কিত সিদ্ধান্ত গ্রহন করেছে, যার ফলেই লক্ষ লক্ষ লোক
+            বিদ্রোহ করছে?
+          </p>
+          <p>
+            He has taken a controversial decision whereupon/whereat millions of
+            people are demonstrating?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">
+            Wherewith/Whereby = যার দ্বারা/মাধ্যমে
+          </p>
+          <p>
+            এটিই হচ্ছে সবচেয়ে ভালো উপায় যার দ্বারা আমরা আমাদের স্বপ্ন বাস্তবায়ন
+            করতে পারি?
+          </p>
+          <p>
+            This is the best means wherewith/whereby we can implement our dream?
+          </p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereto/Whereunto = যে জিনিসের প্রতি</p>
+          <p>যে জিনিসের প্রতি সে ও তার মা অন্ধ, তা আমি জানিনা?</p>
+          <p>I don't know whereto/whereunto she and her mother are blind?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">Whereof = যে বিষয়ে</p>
+          <p>যে বিষয়ে তারা কথা বলছে, তা আমি জানিনা?</p>
+          <p>I don't know whereof they are talking?</p>
+        </div>
+        <div>
+          <p className="text-blue-500">
+            Wherewithal = অর্থনৈতিক উপায় / প্রয়োজনীয় অর্থ
+          </p>
+          <p>
+            আমার একটি ক্যামেরা কেনা দরকার কিন্তু আমার কাছে প্রয়োজনীয় অর্থ নেই?
+          </p>
+          <p>I need to buy a camera but I don't have wherewithal?</p>
         </div>
       </div>
     </div>
