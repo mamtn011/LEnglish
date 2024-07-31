0d904bb6d9f0a4eef61008a625842c22f85fc5ee
Mobin <mamtn011@gmail.com>
2024-08-01 00:45:18 +0600

    modified should

--- src/components/modals/Should.js
+++ src/components/modals/Should.js
@@ -6,81 +6,446 @@ const Should = () => {
         <div>
           <p className="font-bold py-2">Description:</p>
           <div className="py-2 text-blue-600">
+            <p>Should + be = কোনকিছু হওয়া উচিৎ/ কোথাও থাকা উচিৎ।</p>
+            <p>
+              Should + have been = কোনকিছু হওয়া উচিৎ ছিল / কোথাও থাকা উচিৎ ছিল।
+            </p>
+            <p>Should + have = কোনকিছু (অধীনে) থাকা উচিৎ।</p>
+            <p>Should + have had = কোনকিছু (অধীনে) থাকা উচিৎ ছিল।</p>
             <p>Should + v1 = কোনকিছু করা উচিৎ।</p>
             <p>Should have + v3 = কোনকিছু করা উচিৎ ছিল।</p>
-            <p>Should be + v+ing = কোনকিছু করে যাওয়া বা করতে থাকা উচিৎ।</p>
+            <p>
+              Should be + v+ing = কোনকিছু করে যাওয়া বা করতে থাকা
+              (নিয়মিত/প্রতিনিয়ত করা) উচিৎ।
+            </p>
+            <p>
+              Should have been + v+ing = কোনকিছু করে যাওয়া বা করতে থাকা
+              (নিয়মিত/প্রতিনিয়ত করা) উচিৎ ছিল।
+            </p>
+            <p>Should be + v3 = কাউকে কোনকিছু করা উচিৎ। (passive)</p>
+            <p>Should have been + v3 = কাউকে কোনকিছু করা উচিৎ ছিল। (passive)</p>
           </div>
         </div>
         <p className="font-bold">Examples:</p>
+        {/* should be / should have been  */}
         <div>
-          <p>আমার ইংরেজি শেখা উচিৎ।</p>
-          <p>I should learn English.</p>
+          <div className="py-2 text-blue-600">
+            <p>Should + be = কোনকিছু হওয়া উচিৎ/ কোথাও থাকা উচিৎ।</p>
+            <p>
+              Should + have been = কোনকিছু হওয়া উচিৎ ছিল / কোথাও থাকা উচিৎ ছিল।
+            </p>
+          </div>
+          <div>
+            <p>তার সৎ হওয়া উচিৎ।</p>
+            <p>He should be honest.</p>
+          </div>
+          <div>
+            <p>আমার আরও ভালো মানুষ হওয়া উচিৎ।</p>
+            <p>I should be a better person.</p>
+          </div>
+          <div>
+            <p>তোমার ডাক্তার হওয়া উচিৎ ছিল।</p>
+            <p>You should have been a doctor.</p>
+          </div>
+          <div>
+            <p>আমার এখন ঢাকা থাকা উচিৎ।</p>
+            <p>I should be in Dhaka now.</p>
+          </div>
+          <div>
+            <p>এখানে কিছু লোক থাকা উচিৎ।</p>
+            <p>There should be some people here.</p>
+          </div>
+          <div>
+            <p>তোমার তখন বাড়িতে থাকা উচিৎ ছিল।</p>
+            <p>You should have been at home at that time.</p>
+          </div>
+          <div>
+            <p>
+              আমাদের শিক্ষার্থীদের প্রতি আরও সহানুভূতিশীল হওয়া উচিত, কারণ তাদের
+              মানসিক স্বাস্থ্য আমাদের দায়িত্ব।
+            </p>
+            <p>
+              We should be more compassionate towards students because their
+              mental well-being is our responsibility.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার নিজের সিদ্ধান্তে আরও দৃঢ় হওয়া উচিত, কারণ এটি তোমাকে জীবনের
+              পথে আরও এগিয়ে নিয়ে যাবে।
+            </p>
+            <p>
+              You should be more decisive in your decisions because it will help
+              you progress further in life.
+            </p>
+          </div>
+          <div>
+            <p>
+              তার নিজের কাজের প্রতি আরও মনোযোগী হওয়া উচিত, কারণ তার উপর নির্ভর
+              করছে অনেক মানুষের ভবিষ্যৎ।
+            </p>
+            <p>
+              He should be more attentive to his work because the future of many
+              people depends on him.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমাদের খাদ্য বর্জ্য কমাতে আরও সচেতন হওয়া উচিত, কারণ এটি পরিবেশের
+              উপর নেতিবাচক প্রভাব ফেলে।
+            </p>
+            <p>
+              We should be more mindful of reducing food waste because it has a
+              negative impact on the environment.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমার আরও ধৈর্যশীল হওয়া উচিত, কারণ সফলতা অর্জনের জন্য এটি একটি
+              গুরুত্বপূর্ণ গুণ।
+            </p>
+            <p>
+              I should be more patient because it is an important quality for
+              achieving success.
+            </p>
+          </div>
+          <div>
+            <p>
+              তার পরীক্ষার জন্য আরও ভালোভাবে প্রস্তুত হওয়া উচিত ছিল, কারণ এটি
+              তার ভবিষ্যতের জন্য গুরুত্বপূর্ণ ছিল।
+            </p>
+            <p>
+              He should have been better prepared for the exam because it was
+              important for his future.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমাদের আরও যত্নশীল হওয়া উচিত ছিল যখন আমরা সিদ্ধান্তগুলো
+              নিচ্ছিলাম।
+            </p>
+            <p>We should have been more careful when making the decisions.</p>
+          </div>
+          <div>
+            <p>
+              তার পরীক্ষার জন্য আরও ভালোভাবে প্রস্তুত হওয়া উচিত ছিল, কারণ এটি
+              তার ভবিষ্যতের জন্য গুরুত্বপূর্ণ ছিল।
+            </p>
+            <p>
+              He should have been better prepared for the exam because it was
+              important for his future.
+            </p>
+          </div>
+        </div>
+
+        {/* should have / should have had  */}
+        <div>
+          <div className="py-2 text-blue-600">
+            <p>Should + have = কোনকিছু (অধীনে) থাকা উচিৎ।</p>
+            <p>Should + have had = কোনকিছু (অধীনে) থাকা উচিৎ ছিল।</p>
+          </div>
+          <div>
+            <p>আমার একটি ঘড়ি থাকা উচিৎ।</p>
+            <p>I should have a watch.</p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি গাড়ি থাকা উচিত, কারণ তুমি প্রায়ই ভ্রমণ করো এবং
+              গণপরিবহন সবসময় সুবিধাজনক নয়।
+            </p>
+            <p>
+              You should have a car because you travel frequently and public
+              transportation is not always convenient.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি ফোন থাকা উচিত, কারণ এটি জরুরি পরিস্থিতিতে সাহায্য চাইতে
+              বা তথ্য পেতে সহায়ক হতে পারে।
+            </p>
+            <p>
+              You should have a phone because it can be helpful for seeking help
+              or getting information in emergencies.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি ল্যাপটপ থাকা উচিত, কারণ এটি তোমাকে যে কোনো জায়গা থেকে
+              কাজ করতে এবং পড়াশোনা করতে দেবে।
+            </p>
+            <p>
+              You should have a laptop because it will allow you to work and
+              study from anywhere.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি ভালো ক্যামেরা থাকা উচিত, কারণ তুমি ফটোগ্রাফির শখ পোষণ
+              করো এবং স্মরণীয় মুহূর্তগুলি ধারণ করতে ভালোবাসো।
+            </p>
+            <p>
+              You should have a good camera because you have a passion for
+              photography and love capturing memorable moments.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি সাইকেল থাকা উচিত, কারণ এটি স্বাস্থ্যের জন্য ভালো এবং
+              কাছাকাছি কোথাও যাওয়ার জন্য সুবিধাজনক।
+            </p>
+            <p>
+              You should have a bicycle because it&apos;s good for health and
+              convenient for getting around nearby places.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি মজবুত ব্যাকপ্যাক থাকা উচিত, কারণ এটি তোমার দৈনন্দিন
+              প্রয়োজনীয় সামগ্রী বহন করার জন্য প্রয়োজনীয়।
+            </p>
+            <p>
+              You should have a sturdy backpack because it&apos;s essential for
+              carrying your daily necessities.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার একটি পরিকল্পনা থাকা উচিত ছিল, কারণ পরিস্থিতি এখন অনেক কঠিন
+              হয়ে পড়েছে।
+            </p>
+            <p>
+              You should have had a plan, because the situation has become very
+              difficult now.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমাদের আরও সময় থাকা উচিত ছিল, কারণ কাজটি এত দ্রুত শেষ করা সম্ভব
+              নয়।
+            </p>
+            <p>
+              We should have had more time, because it&apos;s not possible to
+              complete the work so quickly.
+            </p>
+          </div>
+          <div>
+            <p>
+              তার আরও অভিজ্ঞতা থাকা উচিত ছিল, কারণ এই প্রকল্পটি পরিচালনা করা
+              জটিল।
+            </p>
+            <p>
+              He should have had more experience, because managing this project
+              is complicated.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার আরও সঞ্চয় থাকা উচিত ছিল, কারণ এখন এই খরচগুলি পরিচালনা করা
+              কঠিন হয়ে যাচ্ছে।
+            </p>
+            <p>
+              You should have had more savings, because it&apos;s becoming
+              difficult to manage these expenses now.
+            </p>
+          </div>
+        </div>
+        {/* should + v1 / should have +v3  */}
+        <div>
+          <div className="py-2 text-blue-600">
+            <p>Should + v1 = কোনকিছু করা উচিৎ।</p>
+            <p>Should have + v3 = কোনকিছু করা উচিৎ ছিল।</p>
+          </div>
+          <div>
+            <p>আমার ইংরেজি শেখা উচিৎ।</p>
+            <p>I should learn English.</p>
+          </div>
+          <div>
+            <p>আমার ইংরেজি শেখা উচিৎ ছিল।</p>
+            <p>I should have learnt English.</p>
+          </div>
+          <div>
+            <p>তোমার উচিৎ কাজটি সতর্কতার সাথে করা।</p>
+            <p>You should do the work carefully.</p>
+          </div>
+          <div>
+            <p>এই অবস্থায় তোমার রোদে দৌড়াদৌড়ি করা উচিৎ না।</p>
+            <p>You should not run in the sun in this situation.</p>
+          </div>
+          <div>
+            <p>তার উচিৎ ছিল মেয়েটিকে বিয়ে করা।</p>
+            <p>He should have married the girl.</p>
+          </div>
+          <div>
+            <p>এই কাজটি তোমার আরও সতর্কতার সাথে করা উচিৎ ছিল।</p>
+            <p>You should have done this work more carefully.</p>
+          </div>
+          <div>
+            <p>দেশের এই নাজুক অবস্থায় তোমার পিতার কিছু একটা করা উচিৎ।</p>
+            <p>
+              Your father should do something in this critical situation of the
+              country.
+            </p>
+          </div>
+          <div>
+            <p>তাদের পিতার উচিৎ ছিল তাদের জন্য কিছু জমি কিনে রাখা।</p>
+            <p>Their father should have bought some land for them.</p>
+          </div>
+          <div>
+            <p>আমাদের চেয়ারম্যান এর উচিৎ আমাদের গ্রামকে দুর্নীতি মুক্ত করা।</p>
+            <p>Our chairman should make our village free from corruption.</p>
+          </div>
+          <div>
+            <p>
+              আমাদের সরকারের উচিৎ আমাদের দেশের লোকজনের জন্য সমান অধিকার নিশ্চিত
+              করা।
+            </p>
+            <p>
+              Our government should ensure equal rights for the people of our
+              country.
+            </p>
+          </div>
+          <div>
+            <p>
+              পরিবেশের ভারসাম্য রক্ষা করতে সরকারের উচিৎ ছিল দেশে অনেক গাছ
+              লাগানো।
+            </p>
+            <p>
+              The government should have planted many trees in the country to
+              maintain the balance of the environment.
+            </p>
+          </div>
+        </div>
+        {/* Should be + v+ing / Should have been + v+ing  */}
+        <div>
+          <div className="py-2 text-blue-600">
+            <p>
+              Should be + v+ing = কোনকিছু করে যাওয়া বা করতে থাকা
+              (নিয়মিত/প্রতিনিয়ত করা) উচিৎ।
+            </p>
+            <p>
+              Should have been + v+ing = কোনকিছু করে যাওয়া বা করতে থাকা
+              (নিয়মিত/প্রতিনিয়ত করা) উচিৎ ছিল।
+            </p>
+          </div>
+          <div>
+            <p>আমার ইংরেজি শিখতে থাকা উচিৎ।</p>
+            <p>I should be learning English.</p>
+          </div>
+          <div>
+            <p>একটি ভালো চাকরীর জন্য তোমার প্রতিনিয়ত আবেদন করে যাওয়া উচিৎ।</p>
+            <p>You should be applying regularly for a good job.</p>
+          </div>
+          <div>
+            <p>দক্ষতা বাড়ানোর জন্য তোমার প্রতিনিয়ত অনুশীলন করা উচিত।</p>
+            <p>You should be practicing regularly to increase your skill.</p>
+          </div>
+          <div>
+            <p>
+              পরীক্ষায় ভালো ফলাফলের জন্য তোমার প্রতিনিয়ত পড়াশোনা করা উচিত।
+            </p>
+            <p>
+              You should be studying regularly to achieve good results in the
+              exams.
+            </p>
+          </div>
+          <div>
+            <p>সুস্থ থাকার জন্য তোমার প্রতিনিয়ত ব্যায়াম করা উচিত।</p>
+            <p>You should be exercising regularly to stay healthy.</p>
+          </div>
+          <div>
+            <p>
+              ভালো সম্পর্ক বজায় রাখার জন্য তোমার প্রতিনিয়ত বন্ধুদের সাথে
+              যোগাযোগ রাখা উচিত।
+            </p>
+            <p>
+              You should be staying in touch regularly with friends to maintain
+              good relationships.
+            </p>
+          </div>
+          <div>
+            <p>
+              ক্লাসে ভালো করার জন্য তোমার প্রতিনিয়ত শিক্ষক থেকে পরামর্শ নেওয়া
+              উচিত।
+            </p>
+            <p>
+              You should be regularly seeking advice from teachers to do well in
+              class.
+            </p>
+          </div>
+          <div>
+            <p>আর্থিক স্থিতিশীলতার জন্য তোমার প্রতিনিয়ত সঞ্চয় করা উচিত।</p>
+            <p>You should be saving regularly for financial stability.</p>
+          </div>
+          <div>
+            <p>
+              আমার আরও বেশী পড়াশোনা করা উচিত ছিল, কারণ পরিক্ষার প্রস্তুতির জন্য
+              সময় খুবই অল্প ছিল।
+            </p>
+            <p>
+              I should have been studying more, because there was very little
+              time to prepare for the exam.
+            </p>
+          </div>
+          <div>
+            <p>
+              তোমার আরো অনুশীলন করা উচিত ছিল, কারণ প্রতিযোগিতাটি গুরুত্বপূর্ণ
+              ছিল।
+            </p>
+            <p>
+              You should have been practicing more, because the competition was
+              important.
+            </p>
+          </div>
+          <div>
+            <p>
+              তার আরো সাবধানতা অবলম্বন করা উচিত ছিল, কারণ রাস্তাটি বিপজ্জনক ছিল।
+            </p>
+            <p>
+              He should have been being more careful, because the road was
+              dangerous.
+            </p>
+          </div>
+          <div>
+            <p>
+              তাদের আরো ঘুমানো উচিত ছিল, কারণ পরের দিন তাদের একটি গুরুত্বপূর্ণ
+              মিটিং ছিল।
+            </p>
+            <p>
+              They should have been sleeping more, because they had an important
+              meeting the next day.
+            </p>
+          </div>
+        </div>
+        {/* ............................ */}
+
+        {/* <div>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>আমার ইংরেজি শেখা উচিৎ ছিল।</p>
-          <p>I should have learnt English.</p>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>আমার ইংরেজি শিখতে থাকা উচিৎ।</p>
-          <p>I should be learning English.</p>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>তোমার উচিৎ কাজটি সতর্কতার সাথে করা।</p>
-          <p>You should do the work carefully.</p>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>এই কাজটি তোমার আরও সতর্কতার সাথে করা উচিৎ ছিল।</p>
-          <p>You should have done this work more carefully.</p>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>একটি ভালো চাকরীর জন্য তোমার প্রতিনিয়ত আবেদন করে যাওয়া উচিৎ।</p>
-          <p>You should be applying regularly for a good job.</p>
+          <p>।</p>
+          <p>.</p>
         </div>
         <div>
-          <p>এই অবস্থায় তোমার রোদে দৌড়াদৌড়ি করা উচিৎ না।</p>
-          <p>You should not run in the sun in this situation.</p>
-        </div>
-        <div>
-          <p>তার উচিৎ ছিল মেয়েটিকে বিয়ে করা।</p>
-          <p>He should have married the girl.</p>
-        </div>
-        <div>
-          <p>দেশের এই নাজুক অবস্থায় তোমার পিতার কিছু একটা করা উচিৎ।</p>
-          <p>
-            Your father should do something in this critical situation of the
-            country.
-          </p>
-        </div>
-        <div>
-          <p>তাদের পিতার উচিৎ ছিল তাদের জন্য কিছু জমি কিনে রাখা।</p>
-          <p>Their father should have bought some land for them.</p>
-        </div>
-        <div>
-          <p>এখানে কিছু লোক থাকা উচিৎ।</p>
-          <p>There should be some people here.</p>
-        </div>
-        <div>
-          <p>আমাদের চেয়ারম্যান এর উচিৎ আমাদের গ্রামকে দুর্নীতি মুক্ত করা।</p>
-          <p>Our chairman should make our village free from corruption.</p>
-        </div>
-        <div>
-          <p>
-            আমাদের সরকারের উচিৎ আমাদেশের লকজনের জন্য সমান অধিকার নিশ্চিত করা।
-          </p>
-          <p>
-            Our government should ensure equal rights for the people of our
-            country.
-          </p>
-        </div>
-        <div>
-          <p>
-            পরিবেশের ভারসাম্য রক্ষা করতে সরকারের উচিৎ ছিল দেশে অনেক গাছ লাগানো।
-          </p>
-          <p>
-            The government should have planted many trees in the country to
-            maintain the balance of the environment.
-          </p>
-        </div>
+          <p>।</p>
+          <p>.</p>
+        </div> */}
       </div>
     </div>
   );
