7c4f71919cd8b230048359e16be96ed2e6a49308
Mobin <mamtn011@gmail.com>
2024-12-01 19:57:51 +0600

    completed would

--- src/components/modals/Would.js
+++ src/components/modals/Would.js
@@ -117,7 +117,8 @@ const Would = () => {
         </div>
         <div className="flex flex-col gap-4">
           <p className="text-blue-500">
-            2nd Conditional Sentence in Imaginary Situation.
+            2nd Conditional Sentence in Imaginary Situation. বর্তমানে কোন
+            অবাস্তব পরিস্থিতি কল্পনা করলে।
           </p>
           <div>
             <p>আমি যদি রাজা হতাম তবে তোমাকে সাহায্য করতাম।</p>
@@ -149,6 +150,10 @@ const Would = () => {
             </p>
           </div>
           <div>
+            <p>তুমি যদি টাকা না পেতে তাহলে আমরা হতাশ হতাম।</p>
+            <p>If you did not get the money, we would be disappointed.</p>
+          </div>
+          <div>
             <p>তুমি যদি বাংলাদেশের প্রধানমন্ত্রি হতে তবে তুমি কী করতে?</p>
             <p>
               What would you do if you were the prime minister of Bangladesh?
@@ -174,35 +179,362 @@ const Would = () => {
             like to বিনয় ও ভদ্রতা প্রকাশ করে।)
           </p>
           <div>
-            <p>।</p>
-            <p>.</p>
+            <p>আমি তোমার সাথে লন্ডনে যেতে চাই।</p>
+            <p>I would like to go to London with you.</p>
+          </div>
+          <div>
+            <p>বাংলাদেশের লোকজন শান্তিতে বসবাস করতে চায়।</p>
+            <p>The people of bangladesh would like to live in peace.</p>
+          </div>
+          <div>
+            <p>আমি উচ্চশিক্ষার জন্য বিদেশে যেতে চাই।</p>
+            <p>I would like to go abroad for higher education.</p>
+          </div>
+          <div>
+            <p>তুমি কি এক কাপ চা পান করতে চাও?</p>
+            <p>Would you like to have a cup of tea?</p>
+          </div>
+          <div>
+            <p>তুমি কি ইংরেজি শিখতে চাও?</p>
+            <p>Would you like to learn English?</p>
+          </div>
+          <div>
+            <p>সে কি বসের সাথে কথা বলতে চায়?</p>
+            <p>Would he like to talk to the boss?</p>
+          </div>
+          <div>
+            <p>তুমি কি আমাদের সাথে স্কুলে যেতে চাওনা?</p>
+            <p>Wouldn&apos;t you like to go to school with us?</p>
+          </div>
+          <div>
+            <p>তুমি কি চাও আমরা কাজটি সম্পন্ন করি?</p>
+            <p>Would you like us to complete the task?</p>
+          </div>
+          <div>
+            <p>তোমার বাবা আমার কাছ থেকে কী জানতে চায়?</p>
+            <p>What would your father like to know from me?</p>
+          </div>
+          <div>
+            <p>তোমার বন্ধু বাংলাদেশে কী করতে চায়?</p>
+            <p>What would your friend like to do in Bangladesh?</p>
+          </div>
+          <div>
+            <p>তুমি তোমার জন্মদিনে কোথায় যেতে চাও?</p>
+            <p>Where would you like to go on your birthday?</p>
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <p className="text-blue-500">
-            2nd Conditional Sentence in Imaginary Situation.
+            ভদ্রভাবে কনকিছু চাই/লাগবে/দরকার বোঝাতে Would like + noun ব্যবহৃত হয়।
           </p>
           <div>
-            <p>।</p>
-            <p>.</p>
+            <p>আমি একটু কফি চাই / আমার একটু কফি লাগবে/দরকার।</p>
+            <p>I would like some coffee.</p>
+          </div>
+          <div>
+            <p>আমার একটু লবন লাগবে।</p>
+            <p>I would like a bit of salt.</p>
+          </div>
+          <div>
+            <p>আপনি কি আরও কিছু আম চান?</p>
+            <p>Would you like some more mangoes?</p>
+          </div>
+          <div>
+            <p>তোমার বান্ধবীর কি আরেকটা শাড়ি লাগবে?</p>
+            <p>Would your friend like one more sari?</p>
+          </div>
+          <div>
+            <p>তার কোন ধরনের ড্রেস লাগবে/পছন্দ?</p>
+            <p>What type of dress would she like?</p>
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <p className="text-blue-500">
-            2nd Conditional Sentence in Imaginary Situation.
+            (বিনয়ের সাথে) আতীতে কোন কাজ করতে চেয়েছিলাম কিন্তু করা হয়নি বোঝাতে
+            Would like to + have + v3 ব্যবহৃত হয়।
           </p>
           <div>
-            <p>।</p>
-            <p>.</p>
+            <p>
+              আমি তোমাকে সাহায্য করতে চেয়েছিলাম কিন্তু আমার যথেষ্ট টাকা ছিলোনা।
+            </p>
+            <p>I would like to have helped you but I had not enough money.</p>
+          </div>
+          <div>
+            <p>
+              আমি তোমার সাথে আমেরিকা যেতে চেয়েছিলাম কিন্তু আমাকে তখন একটি জরুরী
+              প্রয়োজনে লন্ডন যেতে হয়েছিলো।
+            </p>
+            <p>
+              I would like to have gone to America with you but I had to go to
+              London for an urgent matter at that time.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমি টিভিতে ফুটবল ম্যাচটি দেখতে চেয়েছিলাম কিন্তু আমাকে বাইরে যেতে
+              হয়েছিলো।
+            </p>
+            <p>
+              I would like to have watched the football match on TV but I had to
+              go out.
+            </p>
+          </div>
+          <div>
+            <p>
+              আমার বন্ধু পরীক্ষায় একটি ভালো রেজাল্ট করতে চেয়েছিল কিন্তু সে
+              অসুস্থতার কারণে পরীক্ষা দিতে পারেনি।
+            </p>
+            <p>
+              My friend would like to have made a good result in the exam but he
+              couldn&apos;t sit for the exam due to illness.
+            </p>
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <p className="text-blue-500">
-            2nd Conditional Sentence in Imaginary Situation.
+            কোন কিছু করতে ভালবাসা/ভাললাগা অথবা ভালো না লাগা/ঘৃণা করা বোঝাতে
+            Would love/hate to + v1 ব্যবহৃত হয়। এখানে Love/hate + v+ing ও
+            ব্যবহার করা যেতে পারে।
           </p>
           <div>
-            <p>।</p>
-            <p>.</p>
+            <p>আমার গল্প বলতে ভালো লাগে / আমি গল্প বলতে ভালবাসি।</p>
+            <p>I would love to tell stories.</p>
+            <p>I love telling stories.</p>
+          </div>
+          <div>
+            <p>আমি নেশা করাকে ঘৃণা করি।</p>
+            <p>I hate taking drags.</p>
+            <p>I would hate to take drags.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            কোন কিছু করতে অধিক পছন্দ করি/করবো (present or future) বোঝাতে Would
+            prefer to + v1 ব্যবহৃত হয়।
+          </p>
+          <div>
+            <p>আমি ক্রিকেট খেলতেই বেশী পছন্দ করি/করবো।</p>
+            <p>I would prefer to play cricket.</p>
+          </div>
+          <div>
+            <p>সে মিথা কথা না বলতেই বেশী পছন্দ করে/করবে।</p>
+            <p>He would prefer not to tell lies.</p>
+          </div>
+          <div>
+            <p>সে সত্য কথা বলার চেয়ে মিথ্যা কথা বলাই বেশী পছন্দ করে/করবে।</p>
+            <p>He would prefer to tell lies rather than speak the truth.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            Polite request এর জন্য Would ব্যবহার করা হয়। এক্ষেত্রে would এর
+            স্থলে could ও ব্যবহার হয়ে থাকে। "Could you" is a more common and
+            versatile choice asking for a small favor or permission. "Would you"
+            is a good choice for making a more significant request or emphasize
+            your willingness to help.
+          </p>
+          <div>
+            <p>দয়া করে তুমি কি দরজাটা খুলবে?</p>
+            <p>Would you open the door, please?</p>
+          </div>
+          <div>
+            <p>আপনি কি আমার সাথে যাবেন?</p>
+            <p>Would you go with me?</p>
+          </div>
+          <div>
+            <p>আপনি কি ব্যাগটা বহন করতে আমাকে সাহায্য করবেন?</p>
+            <p>Would you help me carry this bag?</p>
+          </div>
+          <div>
+            <p>তুমি কি দয়া করে আমার জন্য এক কাপ চা বানাবে?</p>
+            <p>Would you please make a cup of tea for me?</p>
+          </div>
+          <div>
+            <p>আপনি কি দয়াকরে আমাকে মসজিদের রাস্তাটা বলবেন?</p>
+            <p>Would you please tell me the way to mosque?</p>
+          </div>
+          <div>
+            <p>দয়াকরে আপনার ফোন নম্বরটা দিবেন কি?</p>
+            <p>Would you please give me your contact number?</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">Would you mind + V+ing</p>
+          <div>
+            <p>কিছু মনে না করলে দরজাটা বন্ধ করবে কি?</p>
+            <p>Would you mind closing the door?</p>
+          </div>
+          <div>
+            <p>কিছু মনে না করলে আজকের রাতের রান্নাটা কি তুমি করবে?</p>
+            <p>Would you mind cooking dinner tonight?</p>
+          </div>
+          <div>
+            <p>কিছু মনে না করলে আমাকে বিষয়টা ব্যাখ্যা করবে কি?</p>
+            <p>Would you mind explaining it to me?</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            ২টি কাজের মধ্যে বরং ১টি করবো, তবুও/কিন্তু অন্যটি করবনা বোঝাতে Would
+            rathe/sooner + v1 + than + v1 ব্যবহৃত হয়। <br />
+            rather-preference between two specific choices. <br />
+            sooner-stronger preference or aversion to one option over another.
+          </p>
+          <div>
+            <p>সে বরং পড়বে তবুও লিখবেনা।</p>
+            <p>He would rather read than write.</p>
+          </div>
+          <div>
+            <p>সে বরং ফেসবুক চালাবে কিন্তু পড়তে বসবেনা।</p>
+            <p>He would rather browse/use facebook than sit to read.</p>
+          </div>
+          <div>
+            <p>আমি বরং মরবো তবুও ভিক্ষা করবনা।</p>
+            <p>I would rather die than beg.</p>
+          </div>
+          <div>
+            <p>আমি বরং মুভি দেখবো তবুও রাতের খাবারের জন্য বাইরে যাবোনা।</p>
+            <p>I would rather watch a movie than go out for dinner.</p>
+          </div>
+          <div>
+            <p>আমি বরং চাকরী ছেড়ে দিব তবুও তার সাথে কাজ করবোনা।</p>
+            <p>I would sooner quit my job than work with him.</p>
+          </div>
+          <div>
+            <p>আমি বরং অপেক্ষা করবো তবুও ভিড় বাসে উঠবোনা।</p>
+            <p>I would rather wait than get into a crowded bus.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">Would rather এর আরও কিছু উদাহরণ।</p>
+          <div>
+            <p>আমি চাই আজ রাতে আপনি আমাদের বাসায় থাকেন।</p>
+            <p>I would rather you stayed at our house tonight.</p>
+          </div>
+          <div>
+            <p>তারা চায় আমি ক্রিকেট খেলি।</p>
+            <p>They would rather I played cricket.</p>
+          </div>
+          <div>
+            <p>তারা চায়না আমি ক্রিকেট খেলি।</p>
+            <p>They would rather I didn&apos;t play cricket.</p>
+          </div>
+          <div>
+            <p>সে চায় আমি তাকে ডিভোর্স দেই।</p>
+            <p>She would sooner I divored her.</p>
+          </div>
+          <div>
+            <p>রাসেদ চায় আমি তার ক্যামেরাটা ফেরত দেই।</p>
+            <p>Rashed would rather I returned his camera.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            আতীতে কোন কিছু করতাম (করতে পারতাম) কিন্তু করিনি বোঝালে Would have +
+            v3 ব্যবহৃত হয়।
+          </p>
+          <div>
+            <p>আমি লোকটিকে অপমান করতাম। (কোন কারণে করিনি)</p>
+            <p>I would have insulted the man.</p>
+          </div>
+          <div>
+            <p>ছেলেটি পরীক্ষায় ভালো রেজাল্ট করতো। (কোন কারণে করতে পারেনি)</p>
+            <p>The boy would have made a good result in the exam.</p>
+          </div>
+          <div>
+            <p>
+              তুমি যদি আমাকে আগে মিথ্যা না বলতে তবে আমি তোমাকে বিশ্বাস করতাম।
+            </p>
+            <p>If you had not lied to me before, I would have believed you.</p>
+          </div>
+          <div>
+            <p>যদি তুমি আসতে তাহলে আমি যেতাম।</p>
+            <p>If you had come, I would have gone.</p>
+          </div>
+          <div>
+            <p>যদি সে আমাকে দাওয়াত করতো তবে আমি সেখানে যেতাম।</p>
+            <p>If he had invited me, I would have gone there.</p>
+          </div>
+          <div>
+            <p>যদি আমি কঠোর পড়াশোনা করতাম তবে আমি ডাক্তার হতাম।</p>
+            <p>If I had studied hard, I would have become a doctor.</p>
+          </div>
+          <div>
+            <p>
+              আমি যদি আর্থিক সংকটের মুখোমুখি না হতাম তবে আমি এই ফ্লাটটি কিনতাম।
+            </p>
+            <p>
+              I would have bought this appartment if I had not faced financial
+              crisis.
+            </p>
+          </div>
+          <div>
+            <p>আমরা যদি ট্যাক্সি নিতাম তাহলে আমরা প্লেন মিস করতামনা।</p>
+            <p>If we had taken a taxi, we would not have missed the plane.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            বর্তমানে কল্পনা করছি কোন কাজ করতে হতো বোঝালে Would have to + v1
+            ব্যবহৃত হয়।
+          </p>
+          <div>
+            <p>আমি যদি শিক্ষক হতাম তবে আমার তাকে পড়াতে হতো।</p>
+            <p>If I were a teacher, I would have to teach him.</p>
+          </div>
+          <div>
+            <p>আজ যদি তুমি না আসতে তাহলে আমার রান্না করতে হতো।</p>
+            <p>If you didn&apos;t come today, I would have to cook.</p>
+          </div>
+          <div>
+            <p>যদি আমার কাছে পয়সা থাকতো তবে আমার এই ঘৃণ্য কাজটি করতে হতোনা।</p>
+            <p>If I had money, I would not have to do this despicable thing.</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            বর্তমানে কল্পনা করছি অতীতে কোন কাজ করতে হতো বোঝালে Would have had to
+            + v1 ব্যবহৃত হয়।
+          </p>
+          <div>
+            <p>
+              যদি আমি এই পরীক্ষায় পাস না করতাম তবে আমাকে পুরনো চাকরীটা করতে হতো।
+            </p>
+            <p>
+              If I had not passed this exam, I would have had to do my old job.
+            </p>
+          </div>
+          <div>
+            <p>
+              যদি আমার বন্ধু আমাকে সাহায্য না করত তবে আমাকে বাড়ি বিক্রি করতে
+              হতো।
+            </p>
+            <p>
+              If my friend had not helped me, I would have had to sell the
+              house.
+            </p>
+          </div>
+          <div>
+            <p>
+              যদি আমার ড্রাইভার গতকাল না আসতো তবে আমাকেই গাড়ি ড্রাইভ করতে হতো।
+            </p>
+            <p>
+              If my driver had not come yesterday, I would have had to drive the
+              car.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-blue-500">
+            নিজের মতামত জানানোর জন্য I would say ব্যবহৃত হয়।
+          </p>
+          <div>
+            <p>আমি তো বলবো এটা সঠিক পদ্ধতি।</p>
+            <p>I would say that is a correct method.</p>
+          </div>
+          <div>
+            <p>আমি তো বলবো তার পদত্যাগ করা উচিৎ।</p>
+            <p>I would say he should resign.</p>
           </div>
         </div>
       </div>
